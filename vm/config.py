f46c7a7d30323d197209bc590da7a27372de8e94
DJ Thornton <thornton_dj@hotmail.com>
2025-07-30 22:27:25 -0500

    feat: :sparkles: python vm creation
    This commit adds the necessary code files to create an azure
    virtual machine. This comes complete with the creation of
    
    - resource group
    - virtual network
    - network security group
    - vm creation
    - vm extension for software installation via public script file

---
+++ vm/config.py
@@ -0,0 +1,18 @@
+from modules.vm import VMSpecs
+import pulumi
+
+config = pulumi.Config()
+
+# Environment configuration
+azure_location: str = config.require("location")
+resource_group_suffix: str = config.require("resource_group_suffix")
+vnet_address_prefixes: list[str] = config.require_object(
+    "vnet_address_prefixes"
+)
+subnet_address_prefixes: list[str] = config.require_object(
+    "subnet_address_prefixes"
+)
+add_my_public_ip_to_nsg: bool = config.require_bool("add_my_public_ip_to_nsg")
+
+# VM specifications
+vm_specs = [VMSpecs(**spec) for spec in config.require_object("vm_specs")]
